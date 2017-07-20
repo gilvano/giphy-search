628451bb698293bc437dcda1fa6f69a4453b6407
gilvano-intellij <g.piontkoski@gmail.com>
2017-07-19 23:39:22 -0300

    Aula 15 - Criando giphy-search-manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {GiphySearchManualComponent} from "./giphy/giphy-search-manual/giphy-search-manual.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, GiphySearchManualComponent
   ],
   imports: [
     BrowserModule
