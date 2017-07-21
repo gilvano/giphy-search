e01bb3042770d5e2bc64441e0c043940fb886d2d
gilvano-intellij <g.piontkoski@gmail.com>
2017-07-20 21:43:54 -0300

    Aula 16 - Refatorando para dividir em módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {GiphySearchManualComponent} from "./giphy/giphy-search-manual/giphy-search-manual.component";
+import {GiphySearchManualModule} from "./giphy/giphy-search-manual/giphy-search-manual.module";
 
 @NgModule({
   declarations: [
-    AppComponent, GiphySearchManualComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GiphySearchManualModule
   ],
   providers: [],
   bootstrap: [AppComponent]
